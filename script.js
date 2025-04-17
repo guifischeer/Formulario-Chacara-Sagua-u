bf8549606644f8998d2861d5873077e221d2e490
guifischeer <guilherme_florent@estudante.sesisenai.org.br>
2025-04-17 11:26:26 -0300

    remever botao

--- script.js
+++ script.js
@@ -2,14 +2,33 @@ document.getElementById("cadastroForm").addEventListener("submit", function(even
     const nome = document.querySelector("input [name= 'nome']");
     const nascimento = document.querySelector("input [name= 'nascimento']");
     const cpf = document.querySelector("input [name= 'cpf']");
-    const CEP = document.querySelector("input [name= 'CEP']");
-    const nascimento = document.querySelector("input [name= 'nascimento']");
-    const nascimento = document.querySelector("input [name= 'nascimento']");
-    const nascimento = document.querySelector("input [name= 'nascimento']");
-    const nascimento = document.querySelector("input [name= 'nascimento']");
-    const nascimento = document.querySelector("input [name= 'nascimento']");
-    const nascimento = document.querySelector("input [name= 'nascimento']");
-    const nascimento = document.querySelector("input [name= 'nascimento']");
+    const CEP = document.querySelector("input [name= 'cep']");
+    const estado = document.querySelector("input [name= 'estado']");
+    const nacionalidade = document.querySelector("input [name= 'nacionalidade']");
+    const rg = document.querySelector("input [name= 'rg']");
+    const genero = document.querySelector("input [name= 'genero']");
+    const estadocivil = document.querySelector("input [name= 'estadocivil']");
+    const cidade = document.querySelector("input [name= 'cidade']");
+    const bairro = document.querySelector("input [name= 'bairro']");
+    const rua = document.querySelector("input [name= 'rua']");
+    const numero = document.querySelector("input [name= 'numero']");
+    const complemento = document.querySelector("input [name= 'complemento']");
+    const telefone = document.querySelector("input [name= 'telefone']");
+    const celular = document.querySelector("input [name= 'celular']");
+    const email = document.querySelector("input [name= 'email']");
+    const profissao = document.querySelector("input [name= 'profissao']");
+    const empresa = document.querySelector("input [name= 'empresa']");
+    const cargo = document.querySelector("input [name= 'cargo']");
+    const experiencia = document.querySelector("input [name= 'experiencia']");
+    const salario = document.querySelector("input [name= 'salario']");
+    const escolaridade = document.querySelector("input [name= 'escolaridade']");
+    const curso = document.querySelector("input [name= 'curso']");
+    const instituicao = document.querySelector("input [name= 'instituicao']");
+    const conclusao = document.querySelector("input [name= 'conclusao']");
+    const instagram = document.querySelector("input [name= 'instagram']");
+    const linkedin = document.querySelector("input [name= 'linkedin']");
+    const hobbies = document.querySelector("input [name= 'hobbies']");
+    const observacoes = document.querySelector("input [name= 'observacoes']");
 
 
 
