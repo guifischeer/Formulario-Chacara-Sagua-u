309747daa0c0455ff469eadcc9130b7316111555
guifischeer <guilherme_florent@estudante.sesisenai.org.br>
2025-04-17 10:51:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("cadastroForm").addEventListener("submit", function(event) {
+    const nome = document.querySelector("input [name= 'nome']");
+    const nascimento = document.querySelector("input [name= 'nascimento']");
+    const cpf = document.querySelector("input [name= 'cpf']");
+    const CEP = document.querySelector("input [name= 'CEP']");
+    const nascimento = document.querySelector("input [name= 'nascimento']");
+    const nascimento = document.querySelector("input [name= 'nascimento']");
+    const nascimento = document.querySelector("input [name= 'nascimento']");
+    const nascimento = document.querySelector("input [name= 'nascimento']");
+    const nascimento = document.querySelector("input [name= 'nascimento']");
+    const nascimento = document.querySelector("input [name= 'nascimento']");
+    const nascimento = document.querySelector("input [name= 'nascimento']");
+
+
+
+});
